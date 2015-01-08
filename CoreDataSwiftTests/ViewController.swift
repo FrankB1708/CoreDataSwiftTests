8e43086839f6c88565b6583a32713f3a284d40e0
Frank <frank.barret@free.fr>
2015-01-08 11:38:26 +0100

    Initial Commit

---
+++ CoreDataSwiftTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSwiftTests
+//
+//  Created by BARRET Frank (MORPHO) on 08/01/2015.
+//  Copyright (c) 2015 Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
